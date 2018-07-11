d23db1778ec64fcbf56ce52b53f7ed8adbf8797d
Rudi B. Svartveit <rudsvar@gmail.com>
2018-07-11 20:45:04 +0200

    Json formatter appears to work now

--- src/Parser/Example.hs
+++ src/Parser/Example.hs
@@ -10,25 +10,28 @@ data Obj
   | List [Obj]
   | Map [(String, Obj)]
 
-instance Show Obj where
-  show obj = pp 0 obj
+parseJson :: String -> IO ()
+parseJson = parse obj
 
-pp :: Int -> Obj -> String
-pp ind (I i)     = show i
-pp ind (S s)     = show s
-pp ind (List xs) =
-  "[\n" ++
-    intercalate ",\n" (map (\x -> replicate (ind+2) ' ' ++ pp (ind+2) x) xs) ++
-  "\n" ++ replicate ind ' ' ++ "]"
-pp ind (Map xs) =
-  "{\n" ++
-    intercalate ",\n" (map (\(x,y) -> replicate (ind+2) ' ' ++ x ++ ":" ++ pp (ind+2) y) xs) ++
-  "\n" ++ replicate ind ' ' ++ "}"
+parseJsonFile :: FilePath -> IO ()
+parseJsonFile f = readFile f >>= parseJson
+
+instance Show Obj where
+  show o = pp 0 4 o
+
+pp :: Int -> Int -> Obj -> String
+pp _ _(I i)     = show i
+pp _ _ (S s)     = show s
+pp ind dInd (List xs) = formatList ind xs (\x -> replicate (ind+dInd) ' ' ++ pp (ind+dInd) dInd x) "[" "]" 
+pp ind dInd (Map xs) = formatList ind xs (\(x,y) -> replicate (ind+dInd) ' ' ++ x ++ " : " ++ pp (ind+dInd) dInd y) "{" "}"
+
+formatList :: Int -> [a] -> (a -> String) -> String -> String -> String
+formatList ind xs f open close = open ++ "\n" ++ intercalate ",\n" (map f xs) ++ "\n" ++ replicate ind ' ' ++ close
 
 obj :: Parser Obj
 obj = Map <$> identifier `mapTo` (int <|> str <|> objList <|> obj) <?> "obj"
 
-int, objList :: Parser Obj
+int, str, objList :: Parser Obj
 int = I <$> intLit <?> "int"
 str = S <$> strLit <?> "str"
-objList = List <$> (list (int <|> str <|> objList <|> obj)) <?> "list"
+objList = List <$> (list int <|> list str <|> list objList <|> list obj) <?> "list"

3062a8ab85cf64813e7519d19976cd416c5044da
Rudi B. Svartveit <rudsvar@gmail.com>
2018-07-13 18:00:38 +0200

    Add QuickCheck dependency, and add some basic tests

--- src/Parser/ParseT.hs
+++ src/Parser/ParseT.hs
@@ -18,6 +18,18 @@ import Control.Monad (void)
 -- but made into its own type to redefine the show instance.
 data Result b a = Err (State b) | Ok (a, State b)
 
+-- | Convert the result to a bool
+resultToBool :: Result b a -> Bool
+resultToBool (Ok _) = True
+resultToBool (Err _) = False
+
+-- | Check that the result is as expected.
+err, ok :: Result b a -> Bool
+err (Err _) = True
+err _ = False
+ok (Ok _) = True
+ok _ = False
+
 -- | The `Show` instance of the `Result`.
 instance (Show b, Show a) => Show (Result b a) where
   show (Ok (x, st)) = show st ++ show x
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,38 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+{-# LANGUAGE TemplateHaskell #-}
+
+import Parser.String
+
+import Test.QuickCheck
+import Test.QuickCheck.All
+
+item_prop :: String -> Bool
+item_prop [] = err $ parse item []
+item_prop xs = ok $ parse item xs
+
+many_prop :: String -> Bool
+many_prop xs = ok $ parse (many item) xs
+
+some_prop :: String -> Bool
+some_prop [] = err $ parse (some item) []
+some_prop xs = ok $ parse (some item) xs
+
+eof_prop :: String -> Bool
+eof_prop [] = ok $ parse eof []
+eof_prop xs = err $ parse eof xs
+
+many_eof_prop :: String -> Bool
+many_eof_prop xs = ok $ parse (many item *> eof) xs
+
+many_item_prop :: String -> Bool
+many_item_prop [] = err $ parse (many item *> item) []
+many_item_prop xs = ok $ parse (many item *> item) xs
+
+alt_prop :: String -> Bool
+alt_prop [] = ok $ parse (void item <|> eof) []
+alt_prop xs = err $ parse (void (many item *> item) <|> many item *> eof) xs
+
+return []
+runTests = $verboseCheckAll
+
+main :: IO Bool
+main = runTests

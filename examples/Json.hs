14220834e1f751d05980d208853c9509a340706a
Rudi B. Svartveit <rudsvar@gmail.com>
2018-11-19 15:57:03 +0100

    Add more examples

---
+++ examples/Json.hs
@@ -0,0 +1,35 @@
+import Parser.Parse
+
+import Data.List (intercalate)
+
+data JSON
+  = Object [(String, JSON)]
+  | List [JSON]
+  | S String
+  | I Integer
+
+instance Show JSON where
+  show = prettyPrint 0
+
+prettyPrint :: Int -> JSON -> String
+prettyPrint _ (I i) = show i
+prettyPrint _ (S s) = show s
+prettyPrint _ (List xs) = "[" ++ intercalate "," (map show xs) ++ "]"
+prettyPrint _ (Object xs) = "{" ++ intercalate "," (map tupleShow xs) ++ "}"
+  where tupleShow (s, js) = s ++ ":" ++ show js 
+
+json :: Parser JSON
+json = object <|> jlist <|> str <|> int
+
+object :: Parser JSON
+object = Object <$> braces (commaSep tuple)
+  where tuple = liftA2 (,) (identifier <* symbol ":") json
+
+jlist :: Parser JSON
+jlist = List <$> brackets (commaSep json)
+
+int :: Parser JSON
+int = I <$> intLit
+
+str :: Parser JSON
+str = S <$> strLit

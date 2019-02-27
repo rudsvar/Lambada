14910de81a50ba1d117147792b86450281044294
Rudi B. Svartveit <rudsvar@gmail.com>
2019-02-27 13:32:15 +0100

    Let Main.hs parse and evaluate a Lambada file

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,23 @@
 module Main where
 
+import Lambada.Lambada
+import System.Environment
+import qualified Data.Map as M
+
 main :: IO ()
-main = putStrLn "Hello, World!"
+main = do
+  args <- getArgs
+  case args of
+    [] -> repl M.empty
+    (file:_) -> do
+      res <- parseLambadaFile file
+      case res of
+        Err e -> print e
+        Ok (e, _) ->
+          case eval M.empty e of
+            Left err -> putStrLn err
+            Right e' -> print e'
+
+
+repl :: Ctx -> IO ()
+repl ctx = putStr "λ " >> getLine >> repl ctx

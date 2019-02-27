053805b291a5194e19719905a170000ab7a0dfbd
Rudi B. Svartveit <rudsvar@gmail.com>
2019-02-27 15:23:18 +0100

    Add missing documentation and clean up

--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,23 @@
+-- | The entry point of Lambada
+
 module Main where
 
 import Lambada.Lambada
 import System.Environment
 import qualified Data.Map as M
 
+-- | Evaluate a file or start the repl
 main :: IO ()
 main = do
   args <- getArgs
   case args of
     [] -> repl M.empty
     (file:_) -> do
-      res <- parseLambadaFile file
-      case res of
-        Err e -> print e
-        Ok (e, _) ->
-          case eval M.empty e of
-            Left err -> putStrLn err
-            Right e' -> print e'
+      content <- readFile file
+      case evalLambada content of
+        Left e -> putStrLn e
+        Right e -> print e
 
-
+-- | Start the Lambada repl
 repl :: Ctx -> IO ()
 repl ctx = putStr "λ " >> getLine >> repl ctx
--- src/Lambada/Eval.hs
+++ src/Lambada/Eval.hs
@@ -1,5 +1,7 @@
+-- | A module for evaluating expressions in Lambada
+
 module Lambada.Eval
-  ( eval
+  ( evaluate
   , Ctx
   ) where
 
@@ -7,10 +9,15 @@ import Lambada.Expr
 import qualified Data.Map as M
 import Control.Applicative (liftA2)
 
+-- | A data type for storing variables and their values
 type Ctx = M.Map String Expr
 
+-- | A function that evaluates an expression
+evaluate :: Expr -> Either String Expr
+evaluate = eval M.empty
+
+-- | A function that evaluates an expression in a given context
 eval :: Ctx -> Expr -> Either String Expr
--- eval  ctx  e | trace (show e) False = undefined
 eval  _  (EInt i) = return $ EInt i
 eval  _  (EStr s) = return $ EStr s
 eval ctx (Var v)  =
@@ -19,15 +26,15 @@ eval ctx (Var v)  =
     Just x -> return x
 eval ctx (Let s e1 e2) = eval (M.insert s e1 ctx) e2
 
--- | Abs
+-- Abs
 eval  _  (Abs s e) = return $ Abs s e
 
--- | Builtin functions
+-- Builtin functions
 eval ctx (App (Var "+") xs) = foldr (liftA2 (+) . eval ctx) (pure (EInt 0)) xs
 eval ctx (App (Var "*") xs) = foldr (liftA2 (*) . eval ctx) (pure (EInt 1)) xs
 eval ctx (App (Var "-") [x]) = negate <$> eval ctx x
 
--- | App
+-- App
 eval ctx (App (Var x) []) = eval ctx (Var x)
 eval ctx (App (Var f) xs) = do
   f' <- eval ctx (Var f)

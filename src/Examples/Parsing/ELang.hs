602a89e68125ad17e7efe0a48d7bd1024551512b
Rudi B. Svartveit <rudsvar@gmail.com>
2019-03-10 14:22:15 +0100

    Add a parser generator and an example language

---
+++ src/Examples/Parsing/ELang.hs
@@ -0,0 +1,103 @@
+-- | An example expression language
+
+module Examples.Parsing.ELang
+  ( module Examples.Parsing.ELang
+  ) where
+
+import Parser.Parse
+import Parser.Generator
+
+import Control.Applicative (liftA2)
+import Data.Function ((&))
+
+-- | A data type representing expressions
+data Expr
+  = Add Expr Expr -- ^ Add two expressions
+  | Sub Expr Expr -- ^ Subtract an expression from another
+  | Mul Expr Expr -- ^ Multiply two expressions
+  | Div Expr Expr -- ^ Divide an expression by another
+  | And Expr Expr -- ^ Logical and
+  | Or  Expr Expr -- ^ Logical or
+  | Exp Expr Expr -- ^ Exponentiation
+  | Negate Expr -- ^ Negate an expression
+  | I Integer -- ^ An integer type
+  | Var String -- ^ A variable
+  deriving Show
+
+-- | Parse expressions
+expr :: Parser Expr
+expr = liftA2 (foldl (&)) term $ many $
+  (word "+" >> flip Add <$> term) <|>
+  (word "-" >> flip Sub <$> term) <|>
+  (word "||" >> flip Or <$> term)
+
+-- | Parse terms
+term :: Parser Expr
+term = liftA2 (foldl (&)) factor $ many $
+  (word "*" >> flip Mul <$> factor) <|>
+  (word "/" >> flip Div <$> factor) <|>
+  (word "^" >> flip Exp <$> factor) <|>
+  (word "&&" >> flip Div <$> factor)
+
+-- | Parse factors
+factor :: Parser Expr
+factor =
+  I <$> intLit <|>
+  (word "-" >> Negate <$> factor) <|>
+  (word "+" >> factor) <|>
+  Var <$> identifier <|>
+  parens expr
+
+-- | Parse whitespace or comments
+whiteSpace :: Parser ()
+whiteSpace = spaces <|> lineComment <|> blockComment
+
+-- | Parse line comments
+lineComment :: Parser ()
+lineComment = void $ try (string "//") >> many (sat (/='\n')) >> char '\n'
+
+-- | Parse nested block comments
+blockComment :: Parser ()
+blockComment = do
+  void (try $ string "/*")
+  void . many $ do
+    res <- (\x y -> [x,y]) <$> item <*> item
+    case res of
+      "*/" -> void empty
+      "/*" -> blockComment
+      _    -> void item
+  void (string "*/")
+
+{-
+   You can also generate some simple parsers by
+   using `Parser.Generator` instead of specifying
+   them manually.
+-}
+
+-- | Language information
+eLang :: LangInfo Expr
+eLang = LangInfo
+  { opTable = binOps
+  , intConstr = I
+  , varConstr = Var
+  , keywords = []
+  , bottomParser = factor
+  }
+
+-- | Operators and their constructors
+binOps :: [[(String, Expr -> Expr -> Expr)]]
+binOps =
+  [ [ ("+",  Add), ("-",  Sub), ("||",  Or) ]
+  , [ ("*",  Mul), ("/",  Div), ("&&",  And) ]
+  , [ ("^",  Exp) ]
+  ]
+
+-- | Auto generated parsers
+langParsers :: GeneratedParsers Expr
+langParsers = generateParsers eLang
+
+-- | Extracting the auto-generated parser
+generatedExpr, generatedVar, generatedInt :: Parser Expr
+generatedExpr = expression langParsers
+generatedVar  = variable langParsers
+generatedInt  = integer langParsers
---
+++ src/Parser/Generator.hs
@@ -0,0 +1,51 @@
+-- | Automatically generate some parsers for a language.
+
+module Parser.Generator
+  ( OpTable
+  , LangInfo (..)
+  , GeneratedParsers (..)
+  , generateParsers
+  ) where
+
+import Parser.Parse
+import Data.Function ((&))
+
+-- | A table of operator symbols and their constructors.
+type OpTable a = [[(String, a -> a -> a)]]
+
+-- | Information about the language.
+-- Used to generate parsers.
+data LangInfo a = LangInfo
+  { opTable :: OpTable a
+  , keywords :: [String]
+  , intConstr :: Integer -> a
+  , varConstr :: String -> a
+  , bottomParser :: Parser a
+  }
+
+-- | A data type containing the generated parsers.
+data GeneratedParsers a = GeneratedParsers
+  { expression :: Parser a
+  , integer :: Parser a
+  , variable :: Parser a
+  }
+
+-- | Generate parsers using the language information.
+generateParsers :: LangInfo a -> GeneratedParsers a
+generateParsers li = GeneratedParsers
+  { integer = intConstr li <$> intLit
+  , variable = fmap (varConstr li) $ identifier >>= \i ->
+      if i `elem` keywords li then empty else pure i
+  , expression = parseLayers (opTable li) (bottomParser li)
+  }
+
+-- | Parse the layers of expressions, like expression, term, factor etc.
+parseLayers :: OpTable a -> Parser a -> Parser a
+parseLayers [] botParser = botParser
+parseLayers (l:ls) p = do
+  let
+    nextLayerOps = parseLayers ls p
+    currentLayerOps = flip map l $ \(s, op) -> word s >> flip op <$> nextLayerOps
+  e <- nextLayerOps
+  es <- many $ choice currentLayerOps
+  return (foldl (&) e es)

76ed34582128aebe07fdc779bcf0ff041c8a184d
Rudi B. Svartveit <rudsvar@gmail.com>
2019-03-26 20:10:05 +0100

    Add more tests

--- test/LambadaParserTests.hs
+++ test/LambadaParserTests.hs
@@ -6,9 +6,20 @@ import Parser.Parse
 import Lambada.Parser
 import Lambada.Expr
 
+isParsedAs :: String -> Expr -> SpecWith (Arg Expectation)
+isParsedAs s e =
+  it ("parses " ++ show s ++ " as " ++ show e) $
+    parse lambada s `shouldBe` Right e
+
 lambadaParserTests :: Spec
-lambadaParserTests =
-  describe "examples" $ do
-    let str = "let x = 5 in x"
-    it ("parses " ++ show str) $
-      parse lambada str `shouldBe` Right (Let "x" (EInt 5) (EVar "x"))
+lambadaParserTests = do
+  describe "single values" $ do
+    "9" `isParsedAs` EInt 9
+    "abc" `isParsedAs` EVar "abc"
+    "\"abc\"" `isParsedAs` EStr "abc"
+  describe "bigger expressions" $ do
+    "\\x . x" `isParsedAs` Abs "x" (EVar "x")
+    "(\\x . x) y" `isParsedAs` App (Abs "x" (EVar "x")) [EVar "y"] []
+    "let x = 5 in x" `isParsedAs` Let "x" (EInt 5) (EVar "x")
+    "let f = \\x . x in f x" `isParsedAs`
+      Let "f" (Abs "x" (EVar "x")) (App (EVar "f") [EVar "x"] [])
